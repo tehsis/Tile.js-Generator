b99fee5b5368b7c5a2b9ce134ef4bb4e9369a362
Pablo Terradillos <tehsis@gmail.com>
2012-05-13 17:59:20 -0300

    Moved virtualCanvas to utils.

--- brush-models.js
+++ brush-models.js
@@ -1,14 +1,25 @@
 (function () {
-  var vcl = {}; // A local virtual class
+  var localBrush = {}; // A local brush class
 
-  vcl.figures = {	
+  localBrush.figures = {	
+    
+    /**
+     * A dot is a square with a defined height and width. 
+     */
     Dot:  Backbone.Model.extend({
+      
+      /**
+       * Deprecated. Must be moved to the view. 
+       */ 
       draw: function(params) {
         context = params.context || params;
         context.fillRect(this.get('x'), this.get('y'), this.get('width'), 
             this.get('height'));    
       },
 
+      /**
+       * Deprecated. Must be moved to the view. 
+       */ 
       clear: function(params) {
         context = params.context || params;
         context.clearRect(this.get('x'), this.get('y'), this.get('width'), 
@@ -27,6 +38,27 @@
       }
     }),
 
+  };
+
+  localBrush.utils = {
+    Grid: Backbone.Model.extend({
+      draw: function() {
+       var context = this.get('canvas').getContext('2d:');
+       for (var y=0; y<=this.height || y<=this.width; y+= this.dotSize) {
+         context.moveTo(0, y);
+         context.lineTo(this.get('width'), y);
+         context.moveTo(y, 0);
+         context.lineTo(y, this.get('height'));
+         context.lineWidth = 1;
+         context.strokeStyle = "#eae6e6";
+         context.stroke();
+       };		
+      },
+    }),
+    
+        /*
+     * A virtualCanvas is a _collection_ of figures. 
+     */
     VirtualCanvas: Base.extend({
       constructor: function(params) {
         this.width = params.width;
@@ -42,13 +74,13 @@
           if (this.dots[i].x === dot.x && this.dots[i].y === dot.y) {
             insert = false;
             break;
-          }   	  
+          }       
         }
         if (insert) {
           this.dots.push(dot);
         } else {
-          this.dots.splice(i,1);   	  
-        }	      
+          this.dots.splice(i,1);      
+        }         
         return insert;
       },
 
@@ -66,22 +98,5 @@
     })
   };
 
-  vcl.utils = {
-    Grid: Backbone.Model.extend({
-      draw: function() {
-       var context = this.get('canvas').getContext('2d:');
-       for (var y=0; y<=this.height || y<=this.width; y+= this.dotSize) {
-         context.moveTo(0, y);
-         context.lineTo(this.get('width'), y);
-         context.moveTo(y, 0);
-         context.lineTo(y, this.get('height'));
-         context.lineWidth = 1;
-         context.strokeStyle = "#eae6e6";
-         context.stroke();
-       };		
-      },
-    })
-  };
-
-  window.brush = vcl;
+  window.brush = localBrush;
 }) ();
